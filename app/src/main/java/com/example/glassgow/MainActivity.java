36198562f242afc5b5aa0b805eab1e10a0ea5941
loris <loris.du.84@outlook.fr>
2020-03-22 22:25:44 +0100

    Speech to text + control button supplémentaire

--- app/src/main/java/com/example/glassgow/MainActivity.java
+++ app/src/main/java/com/example/glassgow/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.glassgow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.glassgow.ui.login.LoginActivity;
 
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.MediaPlayer;
 import org.videolan.libvlc.Media;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,35 +31,134 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer vlc;
     private LibVLC libVLC;
     private String url = "http://192.168.1.24:8080/0.mp3";
-    private Button button;
-    private Button nextBtn;
+
+    private Button playBtn;
+    private Button pauseBtn;
+    private Button stopBtn;
+    private Button resetBtn;
+
+    private ImageButton imageButton;
+
+    private TextView textView;
+    private TextView textOutput;
+
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button3);
-        nextBtn = findViewById(R.id.button2);
+        playBtn = findViewById(R.id.playBtn);
+        pauseBtn = findViewById(R.id.pauseBtn);
+        stopBtn = findViewById(R.id.stopBtn);
+        resetBtn = findViewById(R.id.resetBtn);
+
+        imageButton = findViewById(R.id.imageButton);
+
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView2);
+        textOutput = findViewById(R.id.textView3);
 
         this.libVLC = new LibVLC(this);
         this.vlc = new MediaPlayer(libVLC);
 
-        Media media = new Media(libVLC, Uri.parse(url));
+        final Media media = new Media(libVLC, Uri.parse(url));
         this.vlc.setMedia(media);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        this.vlc.setEventListener(new MediaPlayer.EventListener() {
+            @Override
+            public void onEvent(MediaPlayer.Event event) {
+                if(event.type == MediaPlayer.Event.TimeChanged){
+                    try{
+                        textView.setText((int) media.getDuration());
+                    } catch (Exception e){
+                        textView.setText(e.getMessage());
+                    }
+
+                }
+            }
+        });
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                vlc.play();
+                textView.setText("Playing");
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vlc.pause();
+                textView.setText("Paused");
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vlc.stop();
+                textView.setText("Stopped");
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vlc.setTime(0);
                 vlc.play();
             }
         });
 
-        nextBtn.setOnClickListener(new View.OnClickListener() {
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                startSpeechToText();
             }
         });
+
+
+    }
+
+
+    private void startSpeechToText() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak something...");
+        try {
+            startActivityForResult(intent, 666);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * Callback for speech recognition activity
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 666: {
+                if (resultCode == RESULT_OK && null != data) {
+                    try{
+                        ArrayList<String> result = data
+                                .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        String text = result.get(0);
+                        textOutput.setText(text);
+                    } catch (Exception e){
+                       textView.setText(e.getMessage());
+                    }
+
+                }
+                break;
+            }
+        }
     }
 }
